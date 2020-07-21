fa7c8697de01b9f769c65388507058e2a9c4f3ae
DESKTOP-5GQCIB0\Grace <grace188039@gmail.com>
2020-07-21 15:48:07 +0800

    test

---
+++ login.php
@@ -0,0 +1,11 @@
+<form action="login_in.php" method="post">
+帳號:<input type="text" name="account"><br>
+密碼:<input type="text" name="password"><br>
+
+<p>身份:</p>
+<p><input type="radio" name="memberradio">會員(員工)</p>
+<p><input type="radio" name="companyradio">廠商</p><br>
+<input type="submit" value="登入">
+</form>	
+
+
